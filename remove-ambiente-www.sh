7d7d124561cca10281d311644de52c2dd31a4697
paulomendes93 <57572577+paulomendes93@users.noreply.github.com>
2019-11-10 23:13:51 -0300

    www

--- remove-ambiente-www.sh
+++ remove-ambiente-www.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # remove o primeiro ambiente:
 # as duas maquinas e a rede
-echo "Parando a maquina nginx"
+echo "Parando a maquina www"
 lxc exec www1 -- /sbin/poweroff
 lxc exec www2 -- /sbin/poweroff
 
 echo "Aguardando 5 segundos para remover"
 sleep 5
 
-echo "Removendo a  maquina  nginx"
+echo "Removendo a  maquina  www"
 lxc delete www1
 lxc delete www2
 
