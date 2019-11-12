418b09a239589a4169a5455108aad4bd305a109e
paulomendes93 <57572577+paulomendes93@users.noreply.github.com>
2019-11-12 20:11:54 -0300

    teste

--- gera-FW.sh
+++ gera-FW.sh
@@ -11,9 +11,9 @@ lxc network create redeFW_DMZ ipv4.address=172.16.0.1/27 ipv4.nat=false ipv4.dhc
 echo "Criando roteador R"
 lxc copy debian9padrao R
 echo "Ligando interfaces"
-lxc network attach redeFW_WEB eth1
-lxc network attach redeFW_SERVERS eth2
-lxc network attach redeFW_DMZ eth3
+lxc network attach redeFW_WEB R eth1
+lxc network attach redeFW_SERVERS R eth2
+lxc network attach redeFW_DMZ R eth3
 
 echo "Copiando configuracoes"
 lxc file push ./conf/R/interfaces R/etc/network/interfaces 
@@ -23,9 +23,6 @@ lxc file push ./conf/R/radvd.conf R/etc/radvd.conf
 
 echo "Iniciando containers"
 lxc start R
-lxc start A1
-lxc start A2
-lxc start B1
 
 echo "Aguardando 10 segundos para garantir que R esta no ar"
 sleep 10
--- gera-www.sh
+++ gera-www.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # gera dois container com nginx e libera acesso para a porta 8081 e 8082
 #
-#echo "Criando redes"
-#lxc network create redeWWW ipv4.address=10.10.80.1/27 ipv4.nat=false ipv4.dhcp=false
-
 echo "Criando o container www1 e www2"
 lxc copy debian9padrao www1
 lxc copy debian9padrao www2
 
-#echo "Ligando interface eth0 na rede interna"
-#lxc network attach redeWWW www1 eth0
-#lxc network attach redeWWW www2 eth0
+echo "Ligando interface eth0 na rede interna"
+lxc network attach redeFW_WEB www1 eth0
+lxc network attach redeFW_WEB www2 eth0
 
 echo "Copiando configuracao de rede www1 e www2"
 lxc file push ./conf/www1/interfaces www1/etc/network/interfaces
@@ -24,20 +21,20 @@ echo "Aguardando 5 segundos para inicialização"
 sleep 5
 
 ### NGINX
-for maq in www1 www2
-do
-	echo "Instalando e configurando $maq"
-	lxc exec $maq -- apt update
-	lxc exec $maq -- apt upgrade -y
-	lxc exec $maq -- apt install -y nginx
-	
-	echo "Apagando arquivo index.html em $maq"
-	lxc file delete $maq/var/www/html/index.html
-	
-	echo "Copiando arquivos exemplo para $maq"
-	lxc file push ./conf/exemplos/index.php $maq/var/www/html/index.php
-	lxc file push ./conf/exemplos/1.php $maq/var/www/html/1.php
-done
+#for maq in www1 www2
+#do
+#	echo "Instalando e configurando $maq"
+#	lxc exec $maq -- apt update
+#	lxc exec $maq -- apt upgrade -y
+#	lxc exec $maq -- apt install -y nginx
+#	
+#	echo "Apagando arquivo index.html em $maq"
+#	lxc file delete $maq/var/www/html/index.html
+#	
+#	echo "Copiando arquivos exemplo para $maq"
+#	lxc file push ./conf/exemplos/index.php $maq/var/www/html/index.php
+#	lxc file push ./conf/exemplos/1.php $maq/var/www/html/1.php
+#done
 
 ###
 # redirecionando a porta 8080 para o servidor na porta 80 no container nginx
