d8eec55f7600c794029f84b2ef481225e14f7454
msciavelli <53977598+msciavelli@users.noreply.github.com>
2019-11-14 21:50:35 -0300

    Update configura-zabbix-server.sh

--- conf/zabbix/configura-zabbix-server.sh
+++ conf/zabbix/configura-zabbix-server.sh
@@ -3,10 +3,19 @@
 echo "Criando o database para o zabbix e preparando o database zabbix com o schema do zabbix-server"
 echo "create database zabbix character set utf8 collate utf8_bin;" | mysql
 echo "grant all privileges on zabbix.* to zabbix@localhost identified by 'inf500';" | mysql 
-zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -uzabbix -pinf500 zabbix
+apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-agent 
+#zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -uzabbix -pinf500 zabbix
 
+mysql -u root -p
+create database zabbix character set utf8 collate utf8_bin;
+grant all privileges on zabbix.* to zabbix@localhost identified by '123456';
 update-rc.d zabbix-server enable
 update-rc.d apache2 enable
+quit;
+
+cd /usr/share/doc/zabbix-server-mysql/ 
+zcat create.sql.gz | mysql -u zabbix -p zabbix 
+
 
 service zabbix-server start
 service apache2 restart
