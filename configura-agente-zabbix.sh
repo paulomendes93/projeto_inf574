f154f86664cb636adb04e6769449f4ffda50c467
paulomendes93 <57572577+paulomendes93@users.noreply.github.com>
2019-11-15 14:18:30 -0300

    teste

---
+++ configura-agente-zabbix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# instala o agente zabbix  nas maquinas 
+for maq in FWEXT ssh proxy FWINT www1 www1 syslog
+do
+    echo "Atualizando pacotes e instalando wget no container " $maq
+    lxc exec $maq -- /usr/bin/apt update
+    lxc exec $maq -- /usr/bin/apt upgrade -y
+    lxc exec $maq -- /usr/bin/apt install -y wget
+
+    echo "Adicionando repositorio zabbix 4.4 no container " $maq
+    lxc exec $maq --  wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+stretch_all.deb
+    lxc exec $maq --  dpkg -i zabbix-release_4.4-1+stretch_all.deb
+    lxc exec $maq -- rm zabbix-release_4.4-1+stretch_all.deb
+    lxc exec $maq -- /usr/bin/apt update
+
+    echo "Instalando agente zabbix no container " $maq
+    lxc exec $maq --  /usr/bin/apt install -y zabbix-agent
+
+    echo "Habilitando agente zabbix para inicilizar automaticamente no container " $maq
+    lxc exec $maq --  update-rc.d zabbix-agent enable
+
+    echo "Copiando o arquivo de configuracao zabbix_server.conf"
+    lxc file push ./conf/zabbix/zabbix_agentd.conf $maq/etc/zabbix/zabbix_agentd.conf --mode 0644
+
+    echo "Iniciando agente zabbix no container " $maq
+    lxc exec $maq -- service zabbix-agent start
+    echo "fim"
+done
---
+++ gera-zabbix.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# documentacao de referencia: https://www.zabbix.com/documentation/4.4/manual
+#
+# gera maquina zabbix
+#
+echo "Copiando sources.list com repositorios non-free"
+lxc file push ./conf/zabbix/sources.list zabbix/etc/apt/sources.list --mode 0755
+
+echo "Atualizando pacotes e instalando wget"
+lxc exec zabbix -- /usr/bin/apt update
+lxc exec zabbix -- /usr/bin/apt upgrade -y
+lxc exec zabbix -- /usr/bin/apt install -y wget
+
+echo "Adicionando repositorio zabbix 4.4"
+lxc exec zabbix --  wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+stretch_all.deb
+lxc exec zabbix --  dpkg -i zabbix-release_4.4-1+stretch_all.deb
+lxc exec zabbix -- rm zabbix-release_4.4-1+stretch_all.deb
+lxc exec zabbix -- /usr/bin/apt update
+
+echo ""
+echo "Pressione ENTER para continuar..."
+read
+
+echo "Instalando zabbix server"
+lxc exec zabbix --  /usr/bin/apt install -y zabbix-server-mysql
+
+echo "Copiando o arquivo de configuracao zabbix_server.conf"
+lxc file push ./conf/zabbix/zabbix_server.conf zabbix/etc/zabbix/zabbix_server.conf --mode 0644
+
+echo "Instalando frontend do zabbix"
+lxc exec zabbix --  /usr/bin/apt install -y zabbix-frontend-php zabbix-apache-conf
+
+echo "Copiando o arquivo de configuracao php.ini"
+lxc file push ./conf/zabbix/php.ini zabbix/etc/php/7.0/apache2/php.ini --mode 0644
+
+echo "Copiando o arquivo configura-zabbix-server.sh"
+lxc file push ./conf/zabbix/configura-zabbix-server.sh zabbix/root/configura-zabbix-server.sh --mode 0755
+
+echo ""
+echo "Pressione ENTER para continuar..."
+read
+
+echo "Instalando o agente zabbix no servidor"
+lxc exec zabbix --  /usr/bin/apt install -y zabbix-agent
