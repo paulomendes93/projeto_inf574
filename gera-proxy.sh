70a835786067ae5192dcaff2022504d0270a38fc
paulomendes93 <57572577+paulomendes93@users.noreply.github.com>
2019-11-15 12:43:00 -0300

    teste

--- gera-proxy.sh
+++ gera-proxy.sh
@@ -10,4 +10,4 @@ done
 
 ###
 # redirecionando a porta 8080 e 8081 para o servidor na porta 80 no container www1 e www2
-lxc config device add proxy myport8080 proxy listen=tcp:0.0.0.0:80 connect=tcp:192.168.0.103:80
+#lxc config device add proxy myport8080 proxy listen=tcp:0.0.0.0:80 connect=tcp:192.168.0.103:80
--- instala-ssh.sh
+++ instala-ssh.sh
@@ -5,8 +5,8 @@ echo "Instalando pacotes no container SSH"
 lxc exec ssh -- /usr/bin/apt install -y openssh-server fail2ban libpam-google-authenticator
 
 echo "Copiando arquivos de configuracao"
-lxc file push conf/ssh/sshd_config ssh/etc/ssh/sshd_config -p --uid 0 --gid 0 --mode 0644
-lxc file push conf/ssh/sshd ssh/etc/pam.d/sshd -p --uid 0 --gid 0 --mode 0644  
+lxc file push conf/ssh/sshd_config 	ssh/etc/ssh/sshd_config -p --uid 0 --gid 0 --mode 0644
+lxc file push conf/ssh/sshd 		ssh/etc/pam.d/sshd -p --uid 0 --gid 0 --mode 0644  
 
 echo "Reiniciando sshd"
 lxc exec ssh -- service sshd restart
